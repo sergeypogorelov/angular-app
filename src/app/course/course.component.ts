13ed868d2ee70a784abaa89f0994b5dde57375de
Sergey Pogorelov <sergey.pogorelov.93@gmail.com>
2018-08-19 20:14:13 +0400

    Add form for courses

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-course',
   templateUrl: './course.component.html',
-  styleUrls: ['./course.component.css']
+  styleUrls: ['./course.component.scss']
 })
 export class CourseComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  get formName() {
+    return this.form.controls.name;
+  }
+
+  get formDuration() {
+    return this.form.controls.duration;
+  }
+
+  get formCreateDate() {
+    return this.form.controls.createDate;
+  }
+
+  constructor(
+    private _formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.form = this._formBuilder.group({
+      name: ['', [Validators.required]],
+      duration: ['', [Validators.required]],
+      createDate: ['', [Validators.required]],
+    });
+  }
+
+  submitForm(form: FormGroup) {
+    console.log(form);
   }
 
 }
